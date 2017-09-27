4f44e76eaa82f3adb69c9e8c58471c45b977ed56
adnanel <adnanel94@gmail.com>
2017-09-27 22:42:38 +0200

    CourseUnit serialization

--- src/zamgerapi/types/CourseUnit.kt
+++ src/zamgerapi/types/CourseUnit.kt
@@ -1,7 +1,16 @@
 package zamgerapi.types
 
 import org.json.JSONObject
+import zamgerapi.utility.getStringOrInt
 
 public class CourseUnit( json : JSONObject ) {
-    
+    val ID = json.getStringOrInt("id")
+    val Code = json.getString("code")
+    val Name = json.getString("name")
+    val Abbreviation = json.getString("abbrev")
+    val ECTS = json.getDouble("ects")
+    val Institution = Institution(json.getJSONObject("Institution"))
+    val LectureHours = json.getDouble("lectureHours")
+    val TutorialHours = json.getDouble("tutorialHours")
+    val PracticeHours = json.getDouble("practiceHours")
 }
