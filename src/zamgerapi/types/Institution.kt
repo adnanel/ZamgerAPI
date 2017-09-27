8a5b50e84b84fdaea9e954e3a1a6a402d2d356d5
adnanel <adnanel94@gmail.com>
2017-09-27 22:43:35 +0200

    Institution serialization

--- src/zamgerapi/types/Institution.kt
+++ src/zamgerapi/types/Institution.kt
@@ -1,2 +1,10 @@
 package zamgerapi.types
 
+import org.json.JSONObject
+import zamgerapi.utility.getStringOrInt
+
+public class Institution( json : JSONObject) {
+    val ID = json.getStringOrInt("id")
+    val Name = json.getString("name")
+    val Abbreviation = json.getString("abbrev")
+}
