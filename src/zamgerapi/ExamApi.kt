f5304c557318d42cb095d2311b954f63410a441c
adnanel <adnanel94@gmail.com>
2017-09-30 23:55:57 +0200

    comment

--- src/zamgerapi/ExamApi.kt
+++ src/zamgerapi/ExamApi.kt
@@ -1,5 +1,5 @@
 package zamgerapi
 
 public class ExamApi(private val zctx : ZamgerContext) {
-
+    // sve funkcije su ili za logovanog korisnika ili za teachera, pa je implementacija ove klase low priority
 }
